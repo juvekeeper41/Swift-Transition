c41d2b20e2da060813cec7b5cdd30e1626c86dd5
juvekeeper41 <juvekeeper41@gmail.com>
2016-10-03 08:05:01 -0600

    Added main screen segue to updated controller.

---
+++ File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Swift Transition
+//
+//  Created by Osborne, Jacob on 10/3/16.
+//  Copyright © 2016 Osborne, Jacob. All rights reserved.
+//
+
+import Foundation
---
+++ UpdatedViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UpdatedViewController.swift
+//  Swift Transition
+//
+//  Created by Osborne, Jacob on 10/3/16.
+//  Copyright © 2016 Osborne, Jacob. All rights reserved.
+//
+
+import UIKit
+
+class UpdatedViewController : UIViewController
+{
+    override func viewDidLoad()
+    {
+        //Load the parenversion of a View.
+        super.viewDidLoad()
+        //Load this page's components.
+    }
+}
