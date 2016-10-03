b9aa48fb173de859265ebf41ff0a3dedfac44578
juvekeeper41 <juvekeeper41@gmail.com>
2016-10-03 07:46:12 -0600

    Created a project to explore transitions in Swift iOS.

---
+++ Swift Transition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Transition
+//
+//  Created by Osborne, Jacob on 10/3/16.
+//  Copyright © 2016 Osborne, Jacob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
